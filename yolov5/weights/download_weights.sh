f8c0d3438047a5740e26c334fb416ad549c26767
shua <jiuerrose@outlook.com>
2021-09-15 06:52:40 +0800

    update weights and its download script

--- yolov5/weights/download_weights.sh
+++ yolov5/weights/download_weights.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-wget https://github.com/ultralytics/yolov5/releases/download/v4.0/yolov5s.pt
+wget https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s.pt
 
 # For other yolov5 models, see: https://github.com/ultralytics/yolov5/releases
